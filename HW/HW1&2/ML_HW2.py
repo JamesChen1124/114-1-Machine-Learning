8d9a1579b03d90bf07c357f31e7e7e0d0799d5b3
JamesChen 陳俊言 <z052340266@gmail.com>
2025-09-23 23:07:45 +0800

    Update ML_HW2.py

--- HW/HW1&2/ML_HW2.py
+++ HW/HW1&2/ML_HW2.py
@@ -5,21 +5,21 @@ N, D = 200, 2
 X = torch.randn(N, D)
 w_true = torch.tensor([2.0, -3.0])
 b_true = 0.5
+
 y = (torch.sigmoid(X @ w_true + b_true) > 0.5).float()
-#Parameters to learn (weights & bias)
+#weight and bias
 w = torch.zeros(D, requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
-#Loss & optimizer
+#logistic loss跟optimizer
 loss_fn = torch.nn.BCEWithLogitsLoss()
 opt = torch.optim.SGD([w, b], lr=0.1)
-#minimize loss to solve w&b
-for epoch in range(100):
+#最小化 loss  解出 w,b
+for epoch in range(200):
     opt.zero_grad()
-    logits = X @ w + b                        
-    loss = loss_fn(logits, y)                
-    loss.backward()                             # ∂loss/∂w, ∂loss/∂b
-    opt.step()                                  # gradient descent update
-
+    logits = X @ w + b
+    loss = loss_fn(logits, y)
+    loss.backward()
+    opt.step()
 with torch.no_grad():
     print("Learned w:", w.numpy())
     print("Learned b:", b.item())
